32a990e5a657e93ee5c3c4a68dfe58e75e41adf4
cch <497047485@qq.com>
2020-06-05 16:58:44 +0800

    添加颜色选择和画笔粗细选择

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 'use strict'
 
 const canvas = document.getElementById('can');
+// [canvas.width, canvas.height] = [document.body.offsetWidth, document.body.offsetHeight];
 const ctx = canvas.getContext('2d');
 let start_x, start_y;
 const pathArr = []; // 存储path每一条路径的数组
 let pathIndex = 0; // 标记当前路径的索引
 let pointNum = 0; // 记录每一条路径的点的个数
-let color; // 画笔颜色
 let padding = 15;
+const colorSelector = document.getElementById('color');
+const showColorDom = document.getElementById('color-show');
+let color = colorSelector.value; // 初始化为默认颜色(黑色)
+showColorDom.value = colorSelector.value;
+
+colorSelector.addEventListener('input', (e) => {
+    color = e.target.value;
+    showColorDom.value = e.target.value;
+});
+
+const widthSelector = document.getElementById('pen-width');
+const showWidthDom = document.getElementById('width-show');
+let lineWidth = widthSelector.value;
+showWidthDom.value = widthSelector.value;
+
+widthSelector.addEventListener('input', e => {
+    lineWidth = e.target.value;
+    showWidthDom.value = widthSelector.value;
+})
+
+showWidthDom.addEventListener('input', e => {
+    lineWidth = e.target.value;
+    widthSelector.value = showWidthDom.value;
+})
 
 canvas.addEventListener('mousedown', function (e) {
     [start_x, start_y] = [e.offsetX, e.offsetY];
     let path = new Path2D();
     pathArr.push(path);
-    let [r, g, b] = [Math.random() * 255, Math.random() * 255, Math.random() * 255];
-    color = `rgb(${r}, ${g}, ${b})`;
+    // let [r, g, b] = [Math.random() * 255, Math.random() * 255, Math.random() * 255];
+    // color = `rgb(${r}, ${g}, ${b})`;
     canvas.onmousemove = function (e2) {
         drawing(e2, path);
         pointNum += 1;
-    };
+    }
 })
 
 document.body.addEventListener('mouseup', function () {
@@ -30,7 +54,7 @@ document.body.addEventListener('mouseup', function () {
 })
 
 document.body.addEventListener('mousemove', function (e) {
-    if (e.offsetX < padding || e.offsetY < padding || e.offsetX > this.offsetWidth - padding || e.offsetY > this.offsetHeight - padding) {
+    if (e.clientX < padding || e.clientY < padding || e.clientX > this.offsetWidth - padding || e.clientY > this.offsetHeight - padding) {
         if (canvas.onmousemove) {
             canvas.onmousemove = null;
             pathIndex += 1;
@@ -47,5 +71,8 @@ function drawing (e, path) {
         path.lineTo(endX, endY);
     }
     ctx.strokeStyle = color;
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
     ctx.stroke(pathArr[pathIndex]);
 }
